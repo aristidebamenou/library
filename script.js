a6ffedd154863d1634b8d5eda0197271c26dc03b
ccelsin <124219444+ccelsin@users.noreply.github.com>
2023-07-09 19:08:12 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let myLibrary = [];
+
+function Book(title, author, pages, isread) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isread = isread;
+  
+}
+
+function addMyBook() {
+    let title = document.getElementById("Title").value;
+    let author = document.getElementById("Author").value;
+    let pages = document.getElementById("Pages").value;
+    let isread = document.getElementById("isread").checked;
+    let bookAdded = Book(title, author, pages, isread);
+  
+}    
+
+let addbutton = document.getElementById("addBook");
+
+addbutton.addEventListener('click', function() {
+    console.log("work");
+    let addForm = document.getElementById("form");
+    addForm.style.display = 'block';
+});
+
+document.querySelector(".form").addEventListener('submit', function(){
+    
+    alert('Hello')
+})

a61c2857ba0580ac78406a87f6bd033b196e082e
aristidebamenou <aristide.bamenou@hotmail.com>
2026-03-04 17:04:43 +0100

    feat(script) : add Book constructor and implement addBookToLibrary

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const myLibrary = [];
 
+function Book(title, author, numberOfPages, read) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+    this.read = read;
+}
+
+function addBookToLibrary(title, author, numberOfPages, read) {
+    let book = new Book(title, author, numberOfPages, read);
+    myLibrary.push(book);
+}

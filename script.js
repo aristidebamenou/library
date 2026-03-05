03a7d087bebc8a426679ecf95d8c5809037446b1
aristidebamenou <aristide.bamenou@hotmail.com>
2026-03-05 17:21:35 +0100

    feat(script) : "New book" button added

--- script.js
+++ script.js
@@ -1,77 +1,114 @@
 import { faker } from "https://esm.sh/@faker-js/faker@v10.3.0";
 
-const myLibrary = [];
-
-createLibrary();
-displayLibrary();
-
 function Book(title, author, numberOfPages, read) {
     this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.numberOfPages = numberOfPages;
     this.read = read;
-}
 
-function createLibrary() {
-    for (let i = 0; i < 15; i++) {
-        addBookToLibrary(faker.book.title(), faker.book.author(), Math.floor(Math.random() * 800), Math.random() < 0.5)
+    this.display = function() {
+        let libraryMain = document.getElementById("library");
+
+        let libraryCol = document.createElement("div");
+        libraryCol.className = "col-md-3 p-2"
+
+        let bookCard = document.createElement("div");
+        bookCard.className = "card book"
+
+        let bookCardBody = document.createElement("div");
+        bookCardBody.className = "card-body";
+
+        let bookCardTitle = document.createElement("h5");
+        bookCardTitle.className = "card-title";
+        bookCardTitle.innerText = this.title;
+
+        let bookCardSubtitle = document.createElement("h6");
+        bookCardSubtitle.className = "card-subtitle mb-2 text-body-secondary";
+        bookCardSubtitle.innerText = `by ${this.author}`;
+
+        let bookCardText = document.createElement("p");
+        bookCardText.className = "card-text";
+        bookCardText.innerText = `${this.numberOfPages} pages`;
+
+        let bookCardFooter = document.createElement("div");
+        bookCardFooter.className = "card-footer text-body-secondary text-end";
+
+        if(this.read) {
+            bookCard.className = `${bookCard.className} border-primary`;
+            bookCardFooter.className = `${bookCardFooter.className} text-bg-primary`;
+            bookCardFooter.innerText = "Read"
+        } else {
+            bookCard.className = `${bookCard.className} border-secondary`;
+            bookCardFooter.className = `${bookCardFooter.className} text-bg-secondary`;
+            bookCardFooter.innerText = "Not read yet"
+        }
+
+        bookCardBody.appendChild(bookCardTitle);
+        bookCardBody.appendChild(bookCardSubtitle);
+        bookCardBody.appendChild(bookCardText);
+        bookCard.appendChild(bookCardBody);
+        bookCard.appendChild(bookCardFooter);
+        libraryCol.appendChild(bookCard);
+        libraryMain.appendChild(libraryCol);
     }
 }
 
-function displayLibrary() {
-    for (let i = 0; i < myLibrary.length; i++) {
-        displayBook(myLibrary[i]);
+function Library() {
+    this.books = [];
+
+    this.display = function() {
+        for (let i = 0; i < this.books.length; i++) {
+            this.books[i].display();
+        }
+    }
+
+    this.addBook = function(title, author, numberOfPages, read) {
+        let book = new Book(title, author, numberOfPages, read);
+        this.books.push(book);
+    }
+
+    this.refresh = function() {
+        let libraryMain = document.getElementById("library");
+        libraryMain.innerHTML = "";
+        this.display();
+    }
+
+    this.initialize = function() {
+        for (let i = 0; i < 3; i++) {
+            this.addBook(faker.book.title(), faker.book.author(), Math.floor(Math.random() * 800), Math.random() < 0.5)
+        }
     }
 }
 
-function addBookToLibrary(title, author, numberOfPages, read) {
-    let book = new Book(title, author, numberOfPages, read);
-    myLibrary.push(book);
+function cleanFormInputs() {
+    let titleInput = document.getElementById("title");
+    let authorInput = document.getElementById("author");
+    let numberOfPagesInput = document.getElementById("numberOfPages");
+    let readInput = document.getElementById("read");
+
+    titleInput.value = "";
+    authorInput.value = "";
+    numberOfPagesInput.value = "";
+    readInput.checked = false;
 }
 
-function displayBook(book) {
-    let libraryMain = document.getElementById("library");
+let myLibrary = new Library();
 
-    let libraryCol = document.createElement("div");
-    libraryCol.className = "col-md-3 p-2"
+let newBookFormClose = document.getElementById("newBookFormClose");
+let newBookForm = document.getElementById("newBookForm");
 
-    let bookCard = document.createElement("div");
-    bookCard.className = "card book"
+myLibrary.initialize();
+myLibrary.refresh();
 
-    let bookCardBody = document.createElement("div");
-    bookCardBody.className = "card-body";
+newBookForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let data = new FormData(newBookForm);
 
-    let bookCardTitle = document.createElement("h5");
-    bookCardTitle.className = "card-title";
-    bookCardTitle.innerText = book.title;
+    myLibrary.addBook(data.get('title'), data.get('author'), data.get('numberOfPages'), data.get('read') === 'on');
+    newBookFormClose.click();
+    myLibrary.refresh();
+    cleanFormInputs();
+})
 
-    let bookCardSubtitle = document.createElement("h6");
-    bookCardSubtitle.className = "card-subtitle mb-2 text-body-secondary";
-    bookCardSubtitle.innerText = `by ${book.author}`;
 
-    let bookCardText = document.createElement("p");
-    bookCardText.className = "card-text";
-    bookCardText.innerText = `${book.numberOfPages} pages`;
-
-    let bookCardFooter = document.createElement("div");
-    bookCardFooter.className = "card-footer text-body-secondary text-end";
-
-    if(book.read) {
-        bookCard.className = `${bookCard.className} border-primary`;
-        bookCardFooter.className = `${bookCardFooter.className} text-bg-primary`;
-        bookCardFooter.innerText = "Read"
-    } else {
-        bookCard.className = `${bookCard.className} border-secondary`;
-        bookCardFooter.className = `${bookCardFooter.className} text-bg-secondary`;
-        bookCardFooter.innerText = "Not read yet"
-    }
-
-    bookCardBody.appendChild(bookCardTitle);
-    bookCardBody.appendChild(bookCardSubtitle);
-    bookCardBody.appendChild(bookCardText);
-    bookCard.appendChild(bookCardBody);
-    bookCard.appendChild(bookCardFooter);
-    libraryCol.appendChild(bookCard);
-    libraryMain.appendChild(libraryCol);
-}
